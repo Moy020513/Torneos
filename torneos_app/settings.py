086b1b5a1a6ae02a55ea08948ce45d10fdf7b5b0
MoisesMartinez1305 <l20590306@sjuanrio.tecnm.mx>
2025-10-14 10:25:59 -0600

    csrf

--- torneos_app/settings.py
+++ torneos_app/settings.py
@@ -99,3 +99,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+# Configuración CSRF para producción
+CSRF_TRUSTED_ORIGINS = [
+    'https://tecnoy.me',
+    'https://www.tecnoy.me',
+]
+
+# Asegurar que CSRF use HTTPS
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+# Configuración adicional para proxies
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
